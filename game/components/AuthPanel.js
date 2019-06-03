a7a4552590fc1e883444d70c1022bbc9e58b6014
lolimay <lolimay@lolimay.cn>
2019-06-04 00:40:45 +0800

    Add inline style for the AuthPanel

--- game/components/AuthPanel.js
+++ game/components/AuthPanel.js
@@ -14,7 +14,7 @@ class AuthPanel extends React.Component {
 
 	renderPanel() {
 		return (
-			<div>
+			<div style={style}>
 				<h3></h3>
 				<p>This is a panel!</p>
 			</div>
@@ -27,5 +27,17 @@ class AuthPanel extends React.Component {
 		return show ? this.renderPanel() : null;
 	}
 }
+
+const style = {
+	position: 'absolute',
+	top: '35%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	background: 'white',
+	zIndex: '1000',
+	padding: 10,
+	boxShadow: '0 0 5px 1px rgb(120, 120, 120)',
+	borderRadius: 5
+};
 
 export default AuthPanel;
