5b8118213a5af072917148a29229b00f85018424
lolimay <lolimay@lolimay.cn>
2019-06-06 01:41:17 +0800

    Add server side action types

---
+++ server/constants/actions.js
@@ -0,0 +1,19 @@
+function createActionTypes(base, types) {
+	const res = {};
+	types.forEach(type => (res[type] = `${ base }_${ type }`));
+	return res;
+}
+
+const APP = [
+	'INITIALIZE_STATE_REQUEST',
+	'INITIALIZE_STATE_SUCCESS',
+	'PLAYER1_JOIN_REQUEST',
+	'PLAYER2_JOIN_REQUEST',
+	'PLAYER1_JOIED',
+	'PLAYER2_JOINED',
+	'PLAYER1_SCORE_REQUEST',
+	'PLAYER2_SCORE_REQUEST',
+	'SCORE_UPDATED'
+];
+
+module.exports = createActionTypes('APP', APP);
