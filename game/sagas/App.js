99615ba0c34e28637824ad8a94e5895a3ccdfe5d
lolimay <lolimay@lolimay.cn>
2019-06-06 00:09:11 +0800

    Add sagas for handlePlayer1JoinRequest

---
+++ game/sagas/App.js
@@ -0,0 +1,25 @@
+import { call, put, takeEvery } from 'redux-saga/effects';
+import { APP } from '../actions/actionsTypes';
+import * as AppActions from '../actions/App';
+import settings from '../constants/settings';
+import io from 'socket.io-client';
+
+const { SERVER_URL } = settings;
+const socket = io.connect(SERVER_URL);
+
+const handlePlayer1JoinRequest = function* handlePlayer1JoinRequest({ payload }) {
+	try {
+		yield call(socket.emit(APP.PLAYER1_JOIN_REQUEST, payload));
+		socket.on(APP.PLAYER1_JOINED, player1 => {
+			put(AppActions.player1Joined(player1));
+		});
+	} catch (err) {
+		console.warn(err);
+	}
+};
+
+const root = function* root() {
+	yield takeEvery(APP.PLAYER1_JOIN_REQUEST, handlePlayer1JoinRequest);
+};
+
+export default root;
