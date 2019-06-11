d3f6448def75b894bdaffc9cc1dbe33468a072a1
lolimay <lolimay@lolimay.cn>
2019-06-11 20:19:25 +0800

    Add last action

--- game/reducers/index.js
+++ game/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authPanel from './authPanel';
 import App from './App';
+import lastAction from './lastAction';
 
 export default combineReducers({
 	App,
-	authPanel
+	authPanel,
+	lastAction
 });
