69f514254e343e11a2d95af19ec850a19a982f5d
lolimay <lolimay@lolimay.cn>
2019-06-12 01:14:46 +0800

    update socketio

--- game/sagas/App.js
+++ game/sagas/App.js
@@ -1,7 +1,8 @@
-import { call, put, takeEvery } from 'redux-saga/effects';
+import { takeEvery } from 'redux-saga/effects';
 import { APP } from '../actions/actionsTypes';
 import * as AppActions from '../actions/App';
 import settings from '../constants/settings';
+import reduxStore from '../lib/createStore';
 import io from 'socket.io-client';
 
 const { SERVER_URL } = settings;
@@ -10,6 +11,10 @@ const socket = io.connect(SERVER_URL);
 const handlePlayer1JoinRequest = function* handlePlayer1JoinRequest({ payload }) {
 	try {
 		yield socket.emit(APP.PLAYER1_JOIN_REQUEST, payload);
+
+		socket.on(APP.PLAYER1_JOINED, player1 => {
+			reduxStore.dispatch(AppActions.player1Joined(player1));
+		});
 	} catch (err) {
 		console.warn(err);
 	}
@@ -18,6 +23,10 @@ const handlePlayer1JoinRequest = function* handlePlayer1JoinRequest({ payload })
 const handlePlayer2JoinRequest = function* handlePlayer2JoinRequest({ payload }) {
 	try {
 		yield socket.emit(APP.PLAYER2_JOIN_REQUEST, payload);
+
+		socket.on(APP.PLAYER2_JOINED, player2 => {
+			reduxStore.dispatch(AppActions.player2Joined(player2));
+		});
 	} catch (err) {
 		console.warn(err);
 	}
--- game/socket.js
+++ game/socket.js
@@ -7,16 +7,6 @@ import settings from './constants/settings';
 const { SERVER_URL } = settings;
 const socket = io.connect(SERVER_URL);
 
-console.dir(socket);
-
-socket.on(APP.PLAYER1_JOINED, player1 => {
-	reduxStore.dispatch(AppActions.player1Joined(player1));
-});
-
-socket.on(APP.PLAYER2_JOINED, player2 => {
-	reduxStore.dispatch(AppActions.player2Joined(player2));
-});
-
 socket.on(APP.STATE_UPDATED, App => {
 	reduxStore.dispatch(AppActions.stateUpdated(App));
 });
