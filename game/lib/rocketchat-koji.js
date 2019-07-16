569d4515fb8c1f3f297f7bbb64c1fd7007db348f
lolimay <1404363070@qq.com>
2019-07-16 23:40:22 +0800

    Add comments for the rocketchat APIs

--- game/lib/rocketchat-koji.js
+++ game/lib/rocketchat-koji.js
@@ -1,3 +1,11 @@
+/**
+ * Obtain context info about the current user. This method receives the current game
+ * name and will return an object containing the current user's username and avatar
+ * URL and the current the opened room's name.
+ * @param {String} appName
+ * @returns {Object} { username : String. avatarUrl : String, roomName : String }
+ */
+
 function getUserInfo(appName) {
 	if (window.parent) {
 		window.parent.postMessage({
@@ -11,6 +19,11 @@ function getUserInfo(appName) {
 	}
 }
 
+/**
+ * Call inviting others modal window to shrare your game link to other channel, room and user.
+ * This method receives the current game and has no return.
+ * @param {String} appName
+ */
 function invitingOthers(appName) {
 	if (window.parent) {
 		window.parent.postMessage({
